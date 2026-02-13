6d22e31e0cdb80aec1eda280a1435d37282c8552
blackrocketlancamentos-commits <blackrocketlancamentos@gmail.com>
2026-02-13 15:07:46 -0300

    Update GOOGLE_SCRIPT_URL to new deployment URL

--- config.ts
+++ config.ts
@@ -5,4 +5,4 @@
 // PASSO FINAL: Depois de seguir o guia, cole a URL do seu App Script implantado aqui embaixo, entre as aspas.
 // Ela deve se parecer com: "https://script.google.com/macros/s/..."
 // FIX: Explicitly typing the constant as a string prevents TypeScript from inferring a too-specific literal type, which causes a comparison error in App.tsx.
-export const GOOGLE_SCRIPT_URL: string = "https://script.google.com/macros/s/AKfycbyK6Sgg-Hp-nt9BNS6tjU1iBXsNt5VVzcP9KOHhtd0GlGWfla2t21yUX_1nA_S5e1qJ/exec";
+export const GOOGLE_SCRIPT_URL: string = "https://script.google.com/macros/s/AKfycbzUptotd8kX2LlQt67R726rKdBGdZfOV5vkaL0M29y5jXx-0hvp6KoLQ3FwCPuba7OD/exec";
